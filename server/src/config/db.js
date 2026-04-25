070b27612a87541dde7262f7cc794c62a4555f66
JoaoAssRego <jrego@id.uff.br>
2026-04-24 22:34:12 -0300

    connection with db made

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -4,10 +4,16 @@ import mongoose from 'mongoose'
 export const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB conectado: " + $(conn.connection.host));
+        console.log(`MongoDB Conectado: ${conn.connection.host}`);
+        
     }
     catch (error){
-        console.error("")
+        console.error("Não foi possível conectar ao Banco de dados!")
+        process.exit(1);
     }
     
 };
+
+mongoose.connection.on('error', (err) => {
+    console.error(`Erro após conexão inicial: ${err}`);
+});
