def724e62135522b6c0b517be57f2881045a5f8e
JoaoAssRego <jrego@id.uff.br>
2026-04-29 17:49:23 -0300

    Building the logic of the server

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 import { connectDB } from './src/config/db.js'
+import product_route from './src/routes/api/products.js'
+import errorhandler from './src/middleware/errorHandler.js'
 import express from 'express'
 import 'dotenv/config'
 
@@ -7,6 +9,12 @@ connectDB();
 const app = express();
 
 const PORT = process.env.PORT || 5000
+
+app.use(express.json()) // Transforma tudo que o express recebe em JSON
+
+app.use("/api/products",product_route); // Recebe o caminho de rotas de produtos
+
+app.use(errorHandler); // Tratamento de erros
 
 app.listen(PORT,() => {
     console.log("Aplicação rodando!");
